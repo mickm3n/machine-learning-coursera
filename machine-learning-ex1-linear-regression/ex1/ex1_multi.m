643ce777c9f71ba7aa9a42c560a0596d6564c9bf
Mick Zhuang <mickjuang@gmail.com>
2017-09-22 00:40:46 +0800

    submit answer for linear regression

--- machine-learning-ex1-linear-regression/ex1/ex1_multi.m
+++ machine-learning-ex1-linear-regression/ex1/ex1_multi.m
@@ -82,22 +82,29 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
+alpha1 = 0.01;
+alpha2 = 0.03;
+alpha3 = 0.1;
 num_iters = 400;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
-[theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+[theta1, J_history1] = gradientDescentMulti(X, y, theta, alpha1, num_iters);
+[theta2, J_history2] = gradientDescentMulti(X, y, theta, alpha2, num_iters);
+[theta3, J_history3] = gradientDescentMulti(X, y, theta, alpha3, num_iters);
 
 % Plot the convergence graph
 figure;
-plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+plot(1:numel(J_history1), J_history1, '-b', 'LineWidth', 2);
+hold on;
+plot(1:numel(J_history2), J_history2, '-r', 'LineWidth', 2);
+plot(1:numel(J_history3), J_history3, '-k', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
 
 % Display gradient descent's result
 fprintf('Theta computed from gradient descent: \n');
-fprintf(' %f \n', theta);
+fprintf(' %f \n', theta1);
 fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
@@ -105,7 +112,7 @@ fprintf('\n');
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 price = 0; % You should change this
-
+price = [1 ([1650 3] - mu) ./ sigma] * theta3
 
 % ============================================================
 
@@ -150,7 +157,7 @@ fprintf('\n');
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
-
+price = [1 1650 3] * theta
 
 % ============================================================
 
